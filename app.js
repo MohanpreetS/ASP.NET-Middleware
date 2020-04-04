13da25f23ad9a18f2a7bd3678fc3c795f7bef207
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-04-04 13:09:04 -0400

    adding player objects and other variables using query selector

---
+++ app.js
@@ -0,0 +1,16 @@
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1Button'),
+    display: document.querySelector('#p1Display')
+}
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2Button'),
+    display: document.querySelector('#p2Display')
+}
+
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#playto');
+let winningScore = 3;
+let isGameOver = false;
+
