fbad840e226f0866ff5165ef443d5794e5060bf2
Mohanpreet <81784401+mps5199@users.noreply.github.com>
2020-04-04 13:09:04 -0400

    add reset function

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-const p1 = {
-    score: 0,
-    button: document.querySelector('#p1Button'),
-    display: document.querySelector('#p1Display')
-}
-const p2 = {
-    score: 0,
-    button: document.querySelector('#p2Button'),
-    display: document.querySelector('#p2Display')
+const btn1 = document.querySelector('#p1Btn');
+const btn2 = document.querySelector('#p2Btn');
+const resetBtn = document.querySelector('#reset');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display');
+const winningScoreSelect = document.querySelector('#playto');
+
+p1Score = 0;
+p2Score = 0;
+
+let isGameOver = false;
+let winningScore = 3;
+
+// function changeScore(player, opponent) {
+//     if 
+// }
+
+winningScoreSelect.addEventListener('change', function () {
+    winningScore = parseInt(this.value);
+    reset();
+})
+
+function reset() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = p1Score;
+    p2Display.textContent = p2Score;
+    btn1.disabled = false;
+    btn2.disabled = false;
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
 }
 
-const resetButton = document.querySelector('#reset');
-const winningScoreSelect = document.querySelector('#playto');
-let winningScore = 3;
-let isGameOver = false;
+
+
 
